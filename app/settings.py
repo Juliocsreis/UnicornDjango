3c9d134872404c3124d8ddcc19a7b5d50510c25b
Julio <juliocsreis@gmail.com>
2019-09-11 17:36:08 -0300

    git ignore na pasta git

--- app/settings.py
+++ app/settings.py
@@ -17,6 +17,7 @@ import psycopg2
 #from unipath import Path
 import django_heroku
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
